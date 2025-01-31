70e306fd0611f349234528384ef8f69b4da54c33
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-01-31 17:46:22 +0530

    added middleware for city-routes

---
+++ src/middlewares/city-middlewares.js
@@ -0,0 +1,17 @@
+const {StatusCodes}=require('http-status-codes');
+
+const {ErrorResponse}=require('../utils/common');
+const AppError = require('../utils/errors/app-error');
+
+function validateCreateRequest(req,res,next){
+    if(!req.body.name){
+        ErrorResponse.message='something went wrong while creating city';
+        ErrorResponse.error=new AppError(['city name was not found in incoming request'],StatusCodes.BAD_REQUEST);
+
+        return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+    }
+    next();
+}
+module.exports={
+    validateCreateRequest
+}
