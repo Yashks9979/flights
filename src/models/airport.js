70b8d4f66597e998e5509b9362eaf9b778018924
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-01-31 21:30:23 +0530

    added association between city and airport

--- src/models/airport.js
+++ src/models/airport.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+        this.belongsTo(models.City,{
+          foreignKey:'cityId',
+          onDelete:'CASCADE',
+          onUpdate:'CASCADE'
+        });
     }
   }
   Airport.init({
@@ -38,3 +42,6 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Airport;
 };
+
+// npx sequelize migration:generate --name update-city-airport-association
+//
